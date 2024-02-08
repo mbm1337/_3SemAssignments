f62539b69f9e8372ab386569eaa1005a01413ad5
mbm1337 <mikkelsen96@live.dk>
2024-02-08 20:59:41 +0100

    Update ExerciseOne.java

--- src/main/java/org/example/week02/ThreadsExercises/ExerciseOne.java
+++ src/main/java/org/example/week02/ThreadsExercises/ExerciseOne.java
@@ -1,19 +1,40 @@
 package org.example.week02.ThreadsExercises;
 
 
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
+
+class Task implements Callable<String> {
+    private char c;
+
+    public Task(char c) {
+        this.c = c;
+    }
+
+    public String call() throws Exception {
+        return c + "" + c + "" + c;
+    }
+}
 
 public class ExerciseOne {
 
     public static void main(String[] args) {
         ExecutorService executor = Executors.newFixedThreadPool(4);
-        for (char c = 'A'; c <= 'Z'; c++) {
-            char currentChar = c;
-            executor.submit(() -> System.out.println(currentChar + "" + currentChar + "" + currentChar));
+        char c = 'A';
+        for (int i = 0; i < 26; i++) {
+            Future<String> future = executor.submit(new Task(c));
+            try {
+                System.out.println(future.get());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            c++;
         }
         executor.shutdown();
+
     }
 
 }
