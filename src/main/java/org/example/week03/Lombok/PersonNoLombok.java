c03cb6637394f96624d1b0fa5342926eafd6dd22
mbm1337 <mikkelsen96@live.dk>
2024-02-15 22:39:20 +0100

    Create PersonNoLombok.java

---
+++ src/main/java/org/example/week03/Lombok/PersonNoLombok.java
@@ -0,0 +1,70 @@
+package org.example.week03.Lombok;
+
+public class PersonNoLombok {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public PersonNoLombok() {
+    }
+
+    public PersonNoLombok(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PersonNoLombok person = (PersonNoLombok) o;
+
+        if (age != person.age) return false;
+        if (firstName != null ? !firstName.equals(person.firstName) : person.firstName != null) return false;
+        return lastName != null ? lastName.equals(person.lastName) : person.lastName == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firstName != null ? firstName.hashCode() : 0;
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + age;
+        return result;
+    }
+
+}
